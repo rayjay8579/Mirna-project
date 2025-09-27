902c64559f161ea0bc42985c57204385f2dbe51f
rayjay8579 <rayjay8579@gmail.com>
2025-09-27 14:59:42 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,1 +1,17 @@
-print("hello world")
+import pandas as pd
+
+
+with open("mirna list", "r") as f:
+    mirna_list = [line.strip() for line in f if line.strip()]
+
+
+encori_data = pd.read_csv("encori raw data", sep="\t")
+
+
+filtered_data = encori_data[encori_data['miRNA'].isin(mirna_list)]
+
+
+filtered_data.to_csv("filtered_encori_data.tsv", sep="\t", index=False)
+
+print("Done! Filtered data saved as 'filtered_encori_data.tsv'")
+
